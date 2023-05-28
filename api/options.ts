231f2ad485f030403c34a10deb682fbf305397e0
liu9293 <liukai9293@qq.com>
2023-05-29 00:00:29 +0800

    update

---
+++ api/options.ts
@@ -0,0 +1,22 @@
+import { makeCompletion } from '../src/openai'
+import { getOptionMessages } from '../src/prompt'
+
+export const config = {
+  runtime: 'edge'  
+}
+
+export default async function handler (req) {
+  const { searchParams } = new URL(req.url)
+  const word = searchParams.get('word')
+
+  const messages = getOptionMessages(word)
+  const completion = await makeCompletion(messages)
+
+  return new Response(
+    JSON.stringify(completion),
+    {
+      status: 200,
+      headers: { 'content-type': 'application/json' },
+    }
+  )
+}
---
+++ src/openai.ts
@@ -0,0 +1,21 @@
+export async function makeCompletion (messages) {
+  const url = 'https://api.openai.com/v1/chat/completions';
+  
+  const requestOptions = {
+    'method': 'POST',
+    'headers': {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+    },
+    'body': JSON.stringify({
+      model: 'gpt-3.5-turbo',
+      temperature: 1,
+      messages,
+    }),
+  }
+
+  const response = await fetch(url, requestOptions)
+  const json = await response.json()
+  const content = JSON.parse(json.choices[0].message?.content || "{}")
+  return content
+}
