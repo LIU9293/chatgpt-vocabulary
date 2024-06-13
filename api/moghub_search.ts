8a0c9c5c4fb6c56cb513fd15e12d52e0b2024f9a
liu9293 <mylock.kai@gmail.com>
2024-06-13 12:30:01 +0800

    up

--- api/moghub_search.ts
+++ api/moghub_search.ts
@@ -1,13 +1,7 @@
 import { querySimiliarDocuments } from "../src/embed/pinecone";
 
-export const config = { runtime: "nodejs" };
-
 export default async function handler(request, response) {
-  // const { searchParams } = new URL(req.url);
   const { text, namespace } = request.query;
-
-  // const text = searchParams.get("text");
-  // const namespace = searchParams.get("namespace");
 
   if (!text) {
     throw new Error("Please provide text in querystring");
@@ -21,9 +15,4 @@ export default async function handler(request, response) {
   const content = docs[0].pageContent;
 
   return response.json({ content });
-
-  // return new Response(JSON.stringify({ content }), {
-  //   status: 200,
-  //   headers: { "content-type": "application/json" },
-  // });
 }
