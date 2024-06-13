562db2d0ea6df6c63f0cb15a290a7433a92fa6d7
liu9293 <mylock.kai@gmail.com>
2024-06-13 12:07:02 +0800

    fix packages

--- src/embed/supabase.ts
+++ src/embed/supabase.ts
@@ -1,42 +1,47 @@
-import 'dotenv/config'
-import { createClient } from '@supabase/supabase-js'
-import { SupabaseVectorStore } from 'langchain/vectorstores/supabase'
-import { OpenAIEmbeddings } from 'langchain/embeddings/openai'
-import { CharacterTextSplitter } from 'langchain/text_splitter'
-import { GitbookLoader } from 'langchain/document_loaders/web/gitbook'
-import { text1, text2, text3, text4 } from '../../data/test'
+import "dotenv/config";
+import { createClient } from "@supabase/supabase-js";
+import { SupabaseVectorStore } from "@langchain/community/vectorstores/supabase";
+import { OpenAIEmbeddings } from "@langchain/openai";
+import { CharacterTextSplitter } from "langchain/text_splitter";
+import { GitbookLoader } from "langchain/document_loaders/web/gitbook";
+import { text1, text2, text3, text4 } from "../../data/test";
 
-const privateKey = process.env.SUPABASE_API_KEY
-const supabaseUrl = process.env.SUPABASE_URL
+const privateKey = process.env.SUPABASE_API_KEY;
+const supabaseUrl = process.env.SUPABASE_URL;
 
-if (!supabaseUrl) throw new Error(`Expected env var SUPABASE_URL`)
+if (!supabaseUrl) throw new Error(`Expected env var SUPABASE_URL`);
 if (!privateKey) throw new Error(`Expected env var SUPABASE_PRIVATE_KEY`);
 
 const splitter = new CharacterTextSplitter({
   chunkSize: 1000,
-  chunkOverlap: 100
-})
+  chunkOverlap: 100,
+});
 
 const embed = async () => {
   try {
-    const client = createClient(supabaseUrl, privateKey)
-    const gitbookLoader = new GitbookLoader('https://docs.mogroom.com', {
-      shouldLoadAllPaths: true
-    })
-  
-    const textDocs = await splitter.createDocuments([text1, text2, text3, text4])
-    const gitbookDocs = await gitbookLoader.loadAndSplit(splitter)
-    const allDocs = [...textDocs, ...gitbookDocs]  
-    
+    const client = createClient(supabaseUrl, privateKey);
+    const gitbookLoader = new GitbookLoader("https://docs.mogroom.com", {
+      shouldLoadAllPaths: true,
+    });
+
+    const textDocs = await splitter.createDocuments([
+      text1,
+      text2,
+      text3,
+      text4,
+    ]);
+    const gitbookDocs = await gitbookLoader.loadAndSplit(splitter);
+    const allDocs = [...textDocs, ...gitbookDocs];
+
     // Load the docs into the vector store
     await SupabaseVectorStore.fromDocuments(
       allDocs,
       new OpenAIEmbeddings({ openAIApiKey: process.env.OPENAI_API_KEY }),
-      { client }
-    )
+      { client },
+    );
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
-embed()
+embed();
--- src/messages.ts
+++ src/messages.ts
@@ -1,38 +1,39 @@
-import { OpenAIEmbeddings } from 'langchain/embeddings/openai'
-import { createClient } from "@supabase/supabase-js"
-import { SupabaseVectorStore } from "langchain/vectorstores/supabase"
+import { OpenAIEmbeddings } from "@langchain/openai";
+import { createClient } from "@supabase/supabase-js";
+import { SupabaseVectorStore } from "@langchain/community/vectorstores/supabase";
 
-export const getMessages = (question: string, context: string) => ([
+export const getMessages = (question: string, context: string) => [
   {
-    "role": "system",
-    "content": "你是一个AI助手，你需要根据用户的提问以及提供的背景信息来回答用户的问题，请用中文做出回应并尽可能提供有用的信息，如果你没有答案，请说我不知道，并让用户使用蘑菇自习室小程序中查看"
+    role: "system",
+    content:
+      "你是一个AI助手，你需要根据用户的提问以及提供的背景信息来回答用户的问题，请用中文做出回应并尽可能提供有用的信息，如果你没有答案，请说我不知道，并让用户使用蘑菇自习室小程序中查看",
   },
   {
-    "role": "assistant",
-    "content": "背景信息: " + context
+    role: "assistant",
+    content: "背景信息: " + context,
   },
   {
-    "role": "user",
-    "content": question
-  }
-])
+    role: "user",
+    content: question,
+  },
+];
 
 export const askCustomerServiceMessages = async (question: string) => {
-  const privateKey = process.env.SUPABASE_API_KEY || ''
-  const supabaseUrl = 'https://pbwomhaljetjeocbllit.supabase.co'
+  const privateKey = process.env.SUPABASE_API_KEY || "";
+  const supabaseUrl = "https://pbwomhaljetjeocbllit.supabase.co";
   const client = createClient(supabaseUrl, privateKey, {
-    auth: { persistSession: false }
-  })
+    auth: { persistSession: false },
+  });
 
   const vectorStore = await SupabaseVectorStore.fromTexts(
     [],
     {},
     new OpenAIEmbeddings(),
-    { client, tableName: "documents" }
-  )
+    { client, tableName: "documents" },
+  );
 
-  const context = await vectorStore.similaritySearch(question, 1)
-  const contextString = context[0].pageContent
-  const messages = getMessages(question, contextString)
-  return messages
-}
+  const context = await vectorStore.similaritySearch(question, 1);
+  const contextString = context[0].pageContent;
+  const messages = getMessages(question, contextString);
+  return messages;
+};
