0ad84cfd92bae62a1c66f8757d19491080c60dcc
liu9293 <mylock.kai@gmail.com>
2024-06-13 12:28:47 +0800

    update

--- api/moghub_search.ts
+++ api/moghub_search.ts
@@ -2,10 +2,12 @@ import { querySimiliarDocuments } from "../src/embed/pinecone";
 
 export const config = { runtime: "nodejs" };
 
-export default async function handler(req) {
-  const { searchParams } = new URL(req.url);
-  const text = searchParams.get("text");
-  const namespace = searchParams.get("namespace");
+export default async function handler(request, response) {
+  // const { searchParams } = new URL(req.url);
+  const { text, namespace } = request.query;
+
+  // const text = searchParams.get("text");
+  // const namespace = searchParams.get("namespace");
 
   if (!text) {
     throw new Error("Please provide text in querystring");
@@ -18,8 +20,10 @@ export default async function handler(req) {
   const docs = await querySimiliarDocuments("moghub", namespace, text);
   const content = docs[0].pageContent;
 
-  return new Response(JSON.stringify({ content }), {
-    status: 200,
-    headers: { "content-type": "application/json" },
-  });
+  return response.json({ content });
+
+  // return new Response(JSON.stringify({ content }), {
+  //   status: 200,
+  //   headers: { "content-type": "application/json" },
+  // });
 }
