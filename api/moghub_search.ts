1f927f529d59159f50cb7b2f08dc0e08ff013d8c
liu9293 <mylock.kai@gmail.com>
2024-06-13 11:59:12 +0800

    update pinecone/langchain

--- api/moghub_search.ts
+++ api/moghub_search.ts
@@ -1,32 +1,25 @@
-import { querySimiliarDocuments } from '../src/embed/pinecone'
-import { makeCompletion } from '../src/openai'
+import { querySimiliarDocuments } from "../src/embed/pinecone";
 
-export const config = { runtime: 'edge' }
+export const config = { runtime: "edge" };
 
-export default async function handler (req) {
-  const { searchParams } = new URL(req.url)
-  const text = searchParams.get('text')
-  const namespace = searchParams.get('namespace')
+export default async function handler(req) {
+  const { searchParams } = new URL(req.url);
+  const text = searchParams.get("text");
+  const namespace = searchParams.get("namespace");
 
   if (!text) {
-    throw new Error('Please provide text in querystring')
+    throw new Error("Please provide text in querystring");
   }
 
   if (!namespace) {
-    throw new Error('Please provide namespace in querystring')
+    throw new Error("Please provide namespace in querystring");
   }
-  
 
-  const docs = namespace === 'wx9949952662bbb0c4'
-    ? await querySimiliarDocuments('mogroom', 'mogroom-user', text)
-    : await querySimiliarDocuments('mogroom', namespace, text)
-  const content = docs[0].pageContent  
+  const docs = await querySimiliarDocuments("moghub", namespace, text);
+  const content = docs[0].pageContent;
 
-  return new Response(
-    JSON.stringify({ content }),
-    {
-      status: 200,
-      headers: { 'content-type': 'application/json' },
-    }
-  )
+  return new Response(JSON.stringify({ content }), {
+    status: 200,
+    headers: { "content-type": "application/json" },
+  });
 }
--- src/embed/pinecone.ts
+++ src/embed/pinecone.ts
@@ -1,54 +1,62 @@
-import { OpenAIEmbeddings } from 'langchain/embeddings/openai'
-import { PineconeClient } from "@pinecone-database/pinecone"
-import { PineconeStore } from "langchain/vectorstores/pinecone"
-import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter'
+import { Pinecone } from "@pinecone-database/pinecone";
+import { OpenAIEmbeddings } from "@langchain/openai";
+import { PineconeStore } from "@langchain/pinecone";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 
-type RawTextData = string[]
+type RawTextData = string[];
 
-const apiKey = process.env.PINECONE_API_KEY
-const environment = process.env.PINECONE_ENVIRONMENT
-if (!apiKey || !environment) {
-  throw new Error('Please provide PINECONE_API_KEY and PINECONE_ENVIRONMENT')
+const apiKey = process.env.PINECONE_API_KEY;
+
+if (!apiKey) {
+  throw new Error("Please provide PINECONE_API_KEY");
 }
 
-export const embedData = async (pineconeIndexName: string, namespace: string, data: RawTextData): Promise<void> => {
-  const client = new PineconeClient();
-  await client.init({ apiKey, environment })
-  const pineconeIndex = client.Index(pineconeIndexName)
+export const embedData = async (
+  pineconeIndexName: string,
+  namespace: string,
+  data: RawTextData,
+): Promise<void> => {
+  const client = new Pinecone({ apiKey });
+  const pineconeIndex = client.Index(pineconeIndexName);
   const splitter = new RecursiveCharacterTextSplitter({
     chunkSize: 1000,
-    chunkOverlap: 200
-  })
+    chunkOverlap: 200,
+  });
 
-  const connectedString = data.join('\n\n')
-  const docs = await splitter.createDocuments([connectedString])
-  
+  const connectedString = data.join("\n\n");
+  const docs = await splitter.createDocuments([connectedString]);
+
   await PineconeStore.fromDocuments(
     docs,
     new OpenAIEmbeddings({ openAIApiKey: process.env.OPENAI_API_KEY }),
-    { pineconeIndex, namespace }
-  )
-}
+    { pineconeIndex, namespace },
+  );
+};
 
-export const querySimiliarDocuments = async (pineconeIndexName: string, namespace: string, queryText: string) => {
-  const client = new PineconeClient();
-  await client.init({ apiKey, environment })
-
-  const pineconeIndex = client.Index(pineconeIndexName)
+export const querySimiliarDocuments = async (
+  pineconeIndexName: string,
+  namespace: string,
+  queryText: string,
+) => {
+  const client = new Pinecone({ apiKey });
+  const pineconeIndex = client.Index(pineconeIndexName);
 
   const vectorStore = await PineconeStore.fromExistingIndex(
     new OpenAIEmbeddings(),
-    { pineconeIndex, namespace }
-  )
-  
-  const results = await vectorStore.similaritySearch(queryText, 2)
-  return results
-}
+    { pineconeIndex, namespace },
+  );
+
+  const results = await vectorStore.similaritySearch(queryText, 2);
+  return results;
+};
 
 // delete all records in one namespace and re-embed
-export const resetDataForNamespace = async (pineconeIndexName: string, namespace: string, data: RawTextData) => {
-  const client = new PineconeClient();
-  await client.init({ apiKey, environment })
-  await client.Index(pineconeIndexName).delete1({ deleteAll: true, namespace })
-  await embedData(pineconeIndexName, namespace, data)
-}
+export const resetDataForNamespace = async (
+  pineconeIndexName: string,
+  namespace: string,
+  data: RawTextData,
+) => {
+  const client = new Pinecone({ apiKey });
+  await client.Index(pineconeIndexName).deleteAll();
+  await embedData(pineconeIndexName, namespace, data);
+};
